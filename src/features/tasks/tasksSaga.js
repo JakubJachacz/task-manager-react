9bc5d73b085fe414ab0aeeb8f10e930fe93fd26b
Jakub Jachacz <hexadoxpl@gmail.com>
2023-04-05 17:23:30 +0200

    added example tasks using saga

---
+++ src/features/tasks/tasksSaga.js
@@ -0,0 +1,26 @@
+import { delay, call, put, debounce, select, takeEvery } from "redux-saga/effects";
+import { fetchExampleTasks, selectTasks, setTasks } from "./tasksSlice";
+import { getExampleTasks } from "./getExampleTasks";
+import { saveTasksInLocalStorage } from "./tasksLocalStorage";
+
+function* fetchExampleTasksHandler() {
+    try {
+        const tasks = yield select(selectTasks);
+        console.log(tasks);
+        yield delay(1000);
+        const exampleTasks = yield call(getExampleTasks);
+        yield put(setTasks(exampleTasks));
+    } catch (error) {
+        yield call(alert, "Coś poszło nie tak");
+    }
+}
+
+function* saveTasksInLocalStorageHandler() {
+    const tasks = yield select(selectTasks);
+    yield call(saveTasksInLocalStorage, tasks);
+}
+
+export function* watchFetchExampleTasks() {
+    yield debounce(2000, fetchExampleTasks.type, fetchExampleTasksHandler);
+    yield takeEvery("*", saveTasksInLocalStorageHandler);
+}
