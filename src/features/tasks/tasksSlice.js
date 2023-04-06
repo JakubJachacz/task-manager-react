16214393ead498e600b625bd4e5f34c98bad3653
Jakub Jachacz <hexadoxpl@gmail.com>
2023-04-06 22:09:06 +0200

    add getTaskById

--- src/features/tasks/tasksSlice.js
+++ src/features/tasks/tasksSlice.js
@@ -28,7 +28,7 @@ const tasksSlice = createSlice({
             }
         },
         fetchExampleTasks: () => { },
-        setTasks: (state, {payload: tasks}) => {
+        setTasks: (state, { payload: tasks }) => {
             state.tasks = tasks;
         },
     },
@@ -49,5 +49,18 @@ export const selectTasks = state => selectTasksState(state).tasks;
 export const selectHideDone = state => selectTasksState(state).hideDone;
 export const selectAreTasksEmpty = state => selectTasks(state).length === 0;
 export const selectIsEveryTaskDone = state => selectTasks(state).every(({ done }) => done);
+
+export const getTaskById = (state, taskId) =>
+    selectTasks(state).find(({ id }) => id === taskId);
+
+export const selectTasksByQuery = (state, query) => {
+    const tasks = selectTasks(state);
+    if (!query || query.trim() === "") {
+        return tasks;
+    }
+
+    return tasks.filter(({ content }) => content.includes(query.trim()));
+}
+
 
 export default tasksSlice.reducer;
