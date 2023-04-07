96d9e8d72c4b402daee30b63af479f0b92da1052
Jakub Jachacz <hexadoxpl@gmail.com>
2023-04-07 22:18:14 +0200

    add new section, delete {location.search}

--- src/features/tasks/TasksPage/index.js
+++ src/features/tasks/TasksPage/index.js
@@ -4,17 +4,24 @@ import TaskList from "./TaskList";
 import Buttons from "./Buttons";
 import Section from "../../../common/Section";
 import Header from "../../../common/Header";
-import Container from "../../../common/Container"; 
+import Container from "../../../common/Container";
+import Search from './Search';
 
 function TasksPage() {
-
-
   return (
     <Container>
-      {location.search}
       <Header title="Lista zadań" />
-      <Section title="Dodaj nowe zadanie"
+      <Section
+        title="Dodaj nowe zadanie"
         body={<Form />} />
+
+      <Section
+        title="Wyszukiwarka"
+        body={
+        <Search />
+        }
+        />
+
       <Section
         title="Lista zadań"
         body={<TaskList />}
