826f33de9a22b69856737e28b8cb0a081ff2dc5c
Jakub Jachacz <hexadoxpl@gmail.com>
2023-04-09 01:59:56 +0200

    add !!task to Section body

--- src/features/tasks/TaskPage/index.js
+++ src/features/tasks/TaskPage/index.js
@@ -1,22 +1,24 @@
 import { useParams } from "react-router-dom";
 import Container from "../../../common/Container";
 import Header from "../../../common/Header";
-import Section from "../../../common/Section"; 
+import Section from "../../../common/Section";
 import { useSelector } from "react-redux";
 import { getTaskById } from "../tasksSlice";
 
 function TaskPage() {
-const { id } = useParams();
-const task = useSelector(state => getTaskById(state, id));
+    const { id } = useParams();
+    const task = useSelector(state => getTaskById(state, id));
 
     return (
         <Container>
             <Header title="Szczegóły zadania" />
             <Section
                 title={task ? task.content : "Nie znaleziono zadania"}
-                body={
-                <><strong>Ukończono:</strong> {task.done ? "Tak" : "Nie"} </>
-            }
+                body={!!task && (
+                    <>
+                        <strong>Ukończono:</strong> {task.done ? "Tak" : "Nie"}
+                    </>
+                )}
             />
         </Container>
     );
