0b79d4c80b9e0e786fce749a538304e2c4aaac23
Jakub Jachacz <hexadoxpl@gmail.com>
2023-04-09 02:01:04 +0200

    add Button styling

--- src/features/tasks/TasksPage/Buttons/styled.js
+++ src/features/tasks/TasksPage/Buttons/styled.js
@@ -5,19 +5,4 @@ export const Wrapper = styled.div`
     flex-wrap: wrap;
     font-size: 15px;
 `;
-
-export const Button = styled.button`
-    background-color: transparent;
-    color: hsl(180, 100%, 25%);
-    border: none;
-    margin: 0 0 0 20px;
-    transition: 0.3s;
-
-    &:hover {
-        color: hsl(180, 100%, 30%); 
-    }
-
-    &:disabled {
-        color: #ccc; 
-    }
-`;
+ 
